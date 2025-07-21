aff8d88dbab0580c0e0856746ea3e9833a620056
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2025-07-21 18:49:07 +0100

    Adding the DATABASE_URL into alembic context.

--- services/user_service/alembic/env.py
+++ services/user_service/alembic/env.py
@@ -63,6 +63,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    # reading the database URL from the config and setting it
+    # in the Alembic context
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
