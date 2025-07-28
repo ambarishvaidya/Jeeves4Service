32f030887f6412d46fb59a47709da577765e090d
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2025-07-28 17:36:28 +0100

    Setting up Base session and engine in database.

--- services/property_service/app/config.py
+++ services/property_service/app/config.py
@@ -2,4 +2,4 @@
 import os
 
 # Database configuration
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://python:python@localhost:5432/J4S.Property_Service")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://python:python@localhost:5432/J4S.Application")
