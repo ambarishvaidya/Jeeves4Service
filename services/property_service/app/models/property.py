68e2dd25799232025ebe2046e5cb3582dd671dfd
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2025-07-28 19:25:21 +0100

    Property models.

---
+++ services/property_service/app/models/property.py
@@ -0,0 +1,35 @@
+from math import floor
+from sqlalchemy import Column, ForeignKey, Integer, String, null
+
+from app.db.base import Base
+
+# Simple table that currently just holds the property and who created it.
+# Name is what will be used to identify the property. We are not as of
+# now interested in the other details.
+class Property(Base):
+    __tablename__ = "property"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    address = Column(String, index=True)
+    created_by = Column(Integer, nullable=True)
+
+# These could be areas instead of rooms. The expected nomenclaure is 
+# ground floor first room, or main living room or main kitchen or 
+# dry balcony or main balcony. 
+class PropertyRooms(Base):
+    __tablename__ = "property_rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    property_id = Column(Integer, ForeignKey("property.id"), nullable=False)
+    room_name = Column(String, index=True, nullable=False)    
+    
+# This is how a property is associated with a user. A user can be associated
+# with multiple properties. This is a many to many relationship.
+class PropertyAssociation(Base):
+    __tablename__ = "property_associations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    property_id = Column(Integer, ForeignKey("property.id"), nullable=False)
+    user_id = Column(Integer, nullable=False)
+        
