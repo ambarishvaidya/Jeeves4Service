3ddbde639a485e56eeff5b573a4ad270cd2d4e4d
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2025-08-09 20:06:48 +0100

    Adding sqlalchemy session and engine.

---
+++ services/inventory_service/app/config.py
@@ -0,0 +1,5 @@
+"""Application configuration module."""
+import os
+
+# Database configuration
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://python:python@localhost:5432/J4S.Application")
