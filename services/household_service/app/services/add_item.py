bd4832f32088d683549b9fb661addf0dcb6abef8
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2025-08-11 23:45:16 +0100

    Installing spacy package downloaded the en_core_web_sm package for lemmenting the text.

--- services/household_service/app/services/add_item.py
+++ services/household_service/app/services/add_item.py
@@ -1,3 +1,4 @@
+import spacy
 from sqlalchemy import func
 from services.household_service.app.dto.household import AddHouseholdItemDTO, HouseholdItemResponseDTO
 from services.household_service.app.models.household import Household
@@ -5,16 +6,24 @@ from services.household_service.app.models.household import Household
 
 class AddItem:
 
+    nlp = spacy.load("en_core_web_sm")
+
     def __init__(self, logger, session):
         self.logger = logger
         self.session = session
+
+    def lemmatize_text(self, text: str) -> str:
+        doc = self.nlp(text)
+        return " ".join([token.lemma_ for token in doc])
 
     def add_household_item(self, request: AddHouseholdItemDTO) -> HouseholdItemResponseDTO:
         self.logger.info(f"Adding household item with request: {request}")
         try:
             # Handle search vector creation when product_name might be None
-            search_text = (request.product_name or '') + ' ' + request.general_name
-            
+            search_text = self.lemmatize_text((request.product_name or '') + ' ' + request.general_name)
+
+            self.logger.info(f"Search text for household item: {search_text}")
+
             household_item  = Household(
                 product_name=request.product_name,
                 general_name=request.general_name,
